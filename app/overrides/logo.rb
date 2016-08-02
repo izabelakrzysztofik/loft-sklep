fbc547e9dd97864b84db33c696a4325904fa4273
izabelakrzysztofik <izabela.krzysztofik@wp.pl>
2016-08-02 23:17:04 +0200

    init

---
+++ app/overrides/logo.rb
@@ -0,0 +1,4 @@
+Deface::Override.new(:virtual_path => "spree/shared/main_nav_bar", 
+                     :name => "logo", 
+                     :replace_contents => "#logo", 
+                     :text => "store")
